e68d087affcdc3b11d725f8c7c76a41a32e809cb
Ankesh-26 <singhankesh9@gmail.com>
2021-01-23 13:00:11 +0530

    Initial commit

---
+++ blog-app/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
